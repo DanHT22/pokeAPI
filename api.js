93b4a79cc8c83888b04d1176fb2afb8d1cb56cc3
DanielaSanchez130571 <daniela130571@gmail.com>
2023-07-11 19:26:01 -0600

    Grafica de Barra

--- api.js
+++ api.js
@@ -19,47 +19,29 @@ function consumir(){
         // weight.push(datos.weight);
         // console.log(height);
         // console.log(weight);
+
+        //Las siguientes dos lineas sirven para imprimir los datos extraidos
         console.log(datos['abilities'][0]['ability']['name']);
+        console.log(datos['base_experience']);
+        //Aquí convertimos esos mismos datos en variables
+        var nom = datos['abilities'][0]['ability']['name'];
+        var baseExp = datos['base_experience'];
+        //Aquí nada mas imprimimos esos mismos datos. Nomas para asegurarnos que si creamos bien las variables 
+        console.log(nom);
+        console.log(baseExp);
+
+        //Grafico burbujas
+        var data = [
+          {
+            x: [nom],
+            y: [baseExp],
+            type: 'bar'
+          }
+        ];
+        //Recordar cambiar el myDiv1 por myDiv2,3 o 4
+        Plotly.newPlot('myDiv1', data);
     })
-    
 }
-
-
-//Grafico burbujas
-var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 11, 12, 13],
-    mode: 'markers',
-    marker: {
-      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-      opacity: [1, 0.8, 0.6, 0.4],
-      size: [40, 60, 80, 100]
-    }
-  };
-  
-  var data = [trace1];
-  
-  var layout = {
-    title: 'Marker Size and Color',
-    showlegend: false,
-    height: 600,
-    width: 600
-  };
-  
-  Plotly.newPlot('myDiv4', data, layout);
-
-// Grafica de barras
-// function graficar (pesos){
-//     var data = [
-//     {
-//         x: pesos,['pesos']
-//         y: 
-       
-//         type: 'bar'
-//     }
-// ];
-// Plotly.newPlot('myDiv1', data);
-// }
 
 
 
